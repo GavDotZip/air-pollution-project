ea00229e83667af0660e58f3884f8ad33dab964c
GavDotZip <gavdotzip@gmail.com>
2024-02-22 23:44:37 +0000

    setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Locations</a></li>
+        <li><a href="#">Vehicles</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
